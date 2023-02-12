8c2c00ff2c8b02dd7134165f5abc7965fd8dab11
vityaman <vityaman.dev@yandex.ru>
2023-02-12 13:30:40 +0300

    [sle] moved diagonal predominance stuff to separate file

--- app/sle/include/method/iteration/solution.h
+++ app/sle/include/method/iteration/solution.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "app/linal/include/matrix.h"
 #include "app/linal/include/vector.h"
+#include "diagonal-predominance.h"
 #include <cmath>
 #include <iostream>
 
@@ -9,41 +10,10 @@ namespace method {
 namespace iteration {
 
 template <typename T, std::size_t N>
-class diagonal_predominant_matrix : public linal::matrix<T, N, N> {
-private:
-  diagonal_predominant_matrix(const linal::matrix<T, N, N> &other)
-      : linal::matrix<T, N, N>(other) {}
-
-public:
-  static diagonal_predominant_matrix make(const linal::matrix<T, N, N> &other) {
-    return diagonal_predominant_matrix<T, N>(other);
-  }
-};
-
-template <typename T, std::size_t N>
-bool has_diagonal_predominance(const linal::matrix<T, N, N> &matrix) {
-  for (std::size_t i = 0; i < N; i++) {
-    if (2 * std::abs(matrix[i][i]) < sum_by_abs(matrix[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-template <typename T, std::size_t N>
 linal::vector<T, N> solve(const diagonal_predominant_matrix<T, N> &a,
                           const linal::vector<T, N> &b) {
   std::cout << "Hello, World" << std::endl;
   return linal::vector<T, N>::zero();
-}
-
-template <typename T, std::size_t N>
-static T sum_by_abs(const linal::vector<T, N> vector) {
-  T sum = 0;
-  for (std::size_t i = 0; i < N; i++) {
-    sum += std::abs(vector[i]);
-  }
-  return sum;
 }
 
 } // namespace iteration
